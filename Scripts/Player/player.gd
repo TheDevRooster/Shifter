a10205296cc5876fa5b87bf0d8b02ba67e3db354
Rooster <rumpyrooster@gmail.com>
2025-11-04 14:18:23 -0500

    adding movement
    Adding movement and basic user input via action

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -5,21 +5,30 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+func _physics_process(_delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var horizontal_direction := Input.get_axis("player_move_left", "player_move_right")
+	if horizontal_direction:
+		velocity.x = horizontal_direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	var vertical_direction := Input.get_axis("player_move_up", "player_move_down")
+	if vertical_direction:
+		velocity.y = vertical_direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
 	move_and_slide()
+	if Input.is_action_just_pressed("player_ability_1"):
+		ability_1()
+	if Input.is_action_just_pressed("player_ability_2"):
+		ability_2()
+
+
+func ability_1():
+	print("woo")
+
+func ability_2():
+	print("woa")
+
